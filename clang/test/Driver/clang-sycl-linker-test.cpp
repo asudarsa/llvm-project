aca480fde9873c3270fe14ebad3172faccb7e963
Arvind Sudarsanam <arvind.sudarsanam@intel.com>
2025-03-13 14:15:10 -0700

    Address review comments
    Signed-off-by: Arvind Sudarsanam <arvind.sudarsanam@intel.com>

--- clang/test/Driver/clang-sycl-linker-test.cpp
+++ clang/test/Driver/clang-sycl-linker-test.cpp
@@ -6,12 +6,12 @@
 // RUN: clang-sycl-linker --dry-run %t_1.bc %t_2.bc -o a.spv 2>&1 \
 // RUN:   | FileCheck %s --check-prefix=SIMPLE
 // SIMPLE: "{{.*}}llvm-link{{.*}}" {{.*}}.bc {{.*}}.bc -o [[FIRSTLLVMLINKOUT:.*]].bc --suppress-warnings
-// SIMPLE-NEXT: LLVM-SPIRV-Backend: input: {{.*}}.bc, output: a.spv
+// SIMPLE-NEXT: SPIR-V Backend: input: {{.*}}.bc, output: a.spv
 //
 // Test that llvm-link is not called when only one input is present.
 // RUN: clang-sycl-linker --dry-run %t_1.bc -o a.spv 2>&1 \
 // RUN:   | FileCheck %s --check-prefix=SIMPLE-NO-LINK
-// SIMPLE-NO-LINK: LLVM-SPIRV-Backend: input: {{.*}}.bc, output: a.spv
+// SIMPLE-NO-LINK: SPIR-V Backend: input: {{.*}}.bc, output: a.spv
 //
 // Test a simple case with device library files specified.
 // RUN: touch %T/lib1.bc
@@ -20,7 +20,7 @@
 // RUN:   | FileCheck %s --check-prefix=DEVLIBS
 // DEVLIBS: "{{.*}}llvm-link{{.*}}" {{.*}}.bc {{.*}}.bc -o [[FIRSTLLVMLINKOUT:.*]].bc --suppress-warnings
 // DEVLIBS-NEXT: "{{.*}}llvm-link{{.*}}" -only-needed [[FIRSTLLVMLINKOUT]].bc {{.*}}lib1.bc {{.*}}lib2.bc -o [[SECONDLLVMLINKOUT:.*]].bc --suppress-warnings
-// DEVLIBS-NEXT: LLVM-SPIRV-Backend: input: [[SECONDLLVMLINKOUT]].bc, output: a.spv
+// DEVLIBS-NEXT: SPIR-V Backend: input: [[SECONDLLVMLINKOUT]].bc, output: a.spv
 //
 // Test a simple case with .o (fat object) as input.
 // TODO: Remove this test once fat object support is added.
